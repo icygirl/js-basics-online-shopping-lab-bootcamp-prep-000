1d13ad4c77919d4fbdf5f057b7e06fedfa086185
Virginia Herron <inadream@gmail.com>
2018-01-16 06:05:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -73,7 +73,8 @@ function placeOrder(cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.");
   } else {
     console.log(`Your total cost is $${total()}, which will be charged the card ${cardNumber}.`);
-    cart = [];  return cart;
+    cart = [];
+    return cart;
   }
 
 }
